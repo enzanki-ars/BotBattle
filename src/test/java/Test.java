46c55852bbedadb87e18b139a2ff3a8f33d35944
Alex Shafer <shafer.alex@gmail.com>
2015-12-10 10:00:44 -0500

    Create Test.java

---
+++ src/test/java/Test.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class Test {
+
+	@Test
+	public void helloWorld() {
+		assertEquals("\"Hello, World!\" should be the same...", "Hello, World!", "Hello, World!");
+	}
+} 
